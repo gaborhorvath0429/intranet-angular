ca341f6419bafb5a502b72ed9459c52d1634f45d
Horvath Gabor <Gabor.Horvath@eos-hungary.hu>
2019-12-17 16:04:04 +0100

    created saved components directive

--- src/app/core/components/grid/directive/saved-views.directive.ts
+++ src/app/core/components/grid/directive/saved-views.directive.ts
@@ -1,5 +1,6 @@
 import { Directive } from '@angular/core'
-import { GridComponent } from '../grid.component';
+import { GridComponent, GridView } from '../grid.component'
+import { Field } from 'src/app/core/model/model.class'
 
 @Directive({
   selector: '[appSavedViews]',
@@ -7,8 +8,89 @@ import { GridComponent } from '../grid.component';
 })
 export class SavedViewsDirective {
 
-  constructor(private host: GridComponent) {
-    host.ngOnInit = () => host.initSavedViews()
+  constructor(host: GridComponent) {
+    host.savedViews = true
+    host.ngOnInit = this.initSavedViews.bind(host)
+    host.createSavedView = this.createSavedView.bind(host)
+    host.selectColumn = this.selectColumn.bind(host)
+    host.isColumnSelected = this.isColumnSelected.bind(host)
+    host.filterColumns = this.filterColumns.bind(host)
+    host.selectView = this.selectView.bind(host)
+    host.deleteSavedView = this.deleteSavedView.bind(host)
+  }
+
+  initSavedViews = function(): void {
+    this.gridViewService.fromEvent('read').subscribe((views: GridView[]) => {
+      console.log(views)
+      this.savedViewCollection = views
+    })
+
+    this.gridViewService.emit('read', {
+      module: this.module,
+      view: 'grid'
+    })
+  }
+
+  createSavedView = function(): void {
+    let sorters = []
+    let filters = []
+    let record = {
+      name: 'test3',
+      columns: this.selectedColumns.map(e => e.name),
+      sorters,
+      filters
+    }
+    if (this.selectedView) Object.assign(record, {id : this.selectedView.id})
+    this.gridViewService.emit(this.selectedView ? 'update' : 'create', {
+      module: this.module,
+      view: 'grid',
+      records: [record]
+    })
+
+    this.gridViewService.emit('read', {
+      module: this.module,
+      view: 'grid'
+    })
+  }
+
+  selectColumn = function(column: Field): void {
+    if (this.selectedColumns.includes(column)) {
+      this.selectedColumns = this.selectedColumns.filter(e => e !== column)
+    } else {
+      this.selectedColumns.push(column)
+    }
+  }
+
+  isColumnSelected = function(column: Field): boolean {
+    return this.selectedColumns.indexOf(column) > -1
+  }
+
+  filterColumns = function(): void {
+    if (!this.selectedColumns.length) {
+      this.hiddenColumns = []
+    } else {
+      this.hiddenColumns = this.model.fields.filter(e => !this.selectedColumns.includes(e))
+    }
+  }
+
+  selectView = function(view: GridView): void {
+    this.selectedView = view
+    this.selectedColumns = []
+    view.columns.forEach(column => this.selectedColumns.push(this.model.fields.find(e => e.name === column)))
+  }
+
+  deleteSavedView = function(): void {
+    console.log(this.selectedView)
+    this.gridViewService.emit('destroy', {
+      module: this.module,
+      view: 'grid',
+      records: [{ id: this.selectedView.id }]
+    })
+    this.selectedView = null
+    this.gridViewService.emit('read', {
+      module: this.module,
+      view: 'grid'
+    })
   }
 
 }
--- src/app/core/components/grid/grid.component.ts
+++ src/app/core/components/grid/grid.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core'
+import { Component, OnInit, Input, ViewChild } from '@angular/core'
 import Model, { Field } from '../../model/model.class'
 import * as moment from 'moment'
 import { GridViewService } from '../../services/socket.service'
 
-interface GridView {
-  id: string
+export interface GridView {
+  id?: string
   name: string
   columns: string[]
   filters: string[]
@@ -22,89 +22,16 @@ export class GridComponent implements OnInit {
   @Input() model: Model
   @Input() height?: number
   @Input() width?: number
-  @Input() savedViews?: boolean
+  @Input() savedViews = false
 
   public savedViewCollection: GridView[]
   public selectedColumns: Field[] = []
   public hiddenColumns: Field[] = []
   public selectedView: GridView
 
-  constructor(private gridViewService: GridViewService) { }
+  constructor(public gridViewService: GridViewService) { }
 
-  ngOnInit(): void {
-    if (this.savedViews) this.initSavedViews()
-  }
-
-  initSavedViews(): void {
-    this.gridViewService.fromEvent('read').subscribe((views: GridView[]) => {
-      console.log(views)
-      this.savedViewCollection = views
-    })
-
-    this.gridViewService.emit('read', {
-      module: this.module,
-      view: 'grid'
-    })
-  }
-
-  createSavedView(): void {
-    let sorters = []
-    let filters = []
-    this.gridViewService.emit('create', {
-      module: this.module,
-      view: 'grid',
-      records: [{
-        name: 'test',
-        columns: this.selectedColumns.map(e => e.name),
-        sorters,
-        filters
-      }]
-    })
-
-    this.gridViewService.emit('read', {
-      module: this.module,
-      view: 'grid'
-    })
-  }
-
-  selectColumn(column: Field): void {
-    if (this.selectedColumns.includes(column)) {
-      this.selectedColumns = this.selectedColumns.filter(e => e !== column)
-    } else {
-      this.selectedColumns.push(column)
-    }
-  }
-
-  isColumnSelected(column: Field): boolean {
-    return this.selectedColumns.indexOf(column) > -1
-  }
-
-  filterColumns(): void {
-    if (!this.selectedColumns.length) {
-      this.hiddenColumns = []
-    } else {
-      this.hiddenColumns = this.model.fields.filter(e => !this.selectedColumns.includes(e))
-    }
-  }
-
-  selectView(view: GridView): void {
-    this.selectedView = view
-    this.selectedColumns = []
-    view.columns.forEach(column => this.selectedColumns.push(this.model.fields.find(e => e.name === column)))
-  }
-
-  deleteSavedView(): void {
-    this.gridViewService.emit('destroy', {
-      module: this.module,
-      view: 'grid',
-      records: [{ id: this.selectedView.id }]
-    })
-    this.selectedView = null
-    this.gridViewService.emit('read', {
-      module: this.module,
-      view: 'grid'
-    })
-  }
+  ngOnInit() {}
 
   formatCellValue(value: any, column: Field): string | number {
     switch (column.type) {
@@ -121,4 +48,11 @@ export class GridComponent implements OnInit {
     }
   }
 
+  // These methods are overridden by SavedViewsDirective
+  createSavedView(): void {}
+  selectColumn(column: Field): void {}
+  isColumnSelected(column: Field) {}
+  filterColumns(): void {}
+  selectView(view: GridView): void {}
+  deleteSavedView(): void {}
 }
